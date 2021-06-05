b6ecdc351cdc5da196fcb6fcd3661186b162d707
Christodoulos Bochalis <bojito90@gmail.com>
2021-06-05 12:26:36 +0300

    Merges video playback

--- video_playback.py
+++ video_playback.py
@@ -20,7 +20,7 @@ frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
-out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
 
 
 # Read until video is completed
