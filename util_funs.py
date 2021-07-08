9da6cf694d12b72270e73951870c67f4b6d2df93
Christodoulos Bochalis <bojito90@gmail.com>
2021-07-08 11:40:57 +0300

    Added some utils funs

--- util_funs.py
+++ util_funs.py
@@ -8,7 +8,7 @@ import urllib
 from pytube import YouTube
 from sklearn.cluster import KMeans
 import glob
-
+ 
 
 color = (255, 150, 0)
 
@@ -210,3 +210,26 @@ def plot_bb_on_img_with_teams(cv2_img, boxes, tolerance = 0.5, show_text=True):
             cv2.putText(cv2_img_bb, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
                 0.5, color, 2)
     return cv2_img_bb
+
+
+
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    img = img.resize((256, 256), Image.BICUBIC)
+    return img
+
+
+def save_img(image_tensor, filename):
+    image_numpy = image_tensor.float().numpy()
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+    image_numpy = image_numpy.clip(0, 255)
+    image_numpy = image_numpy.astype(np.uint8)
+    image_pil = Image.fromarray(image_numpy)
+    image_pil.save(filename)
+    print("Image saved as {}".format(filename))
